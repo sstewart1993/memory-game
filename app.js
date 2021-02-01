b55ab15ccae9506aeab7b7d61b1d98d2b7d887e1
codeclanstudent <sstewart1993@outlook.com>
2021-02-01 15:17:08 +0000

    basic game needs more css

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // Card options
 const cardArray = [
-{name:'cheesburger', 
-img: 'images/cheesburger.png'
+{name:'cheeseburger', 
+img: 'images/cheeseburger.png'
 },
 {name:'fries', 
 img: 'images/fries.png'
@@ -20,8 +20,8 @@ img: 'images/milkshake.png'
 {name:'pizza', 
 img: 'images/pizza.png'
 },
-{name:'cheesburger', 
-img: 'images/cheesburger.png'
+{name:'cheeseburger', 
+img: 'images/cheeseburger.png'
 },
 {name:'fries', 
 img: 'images/fries.png'
@@ -40,10 +40,14 @@ img: 'images/pizza.png'
 }
 ]
 
+cardArray.sort(() => 0.5 - Math.random())
+
 
 const grid = document.querySelector('.grid')
+const resultDisplay = document.querySelector('#result')
 var cardsChosen = []
 var cardsChosenId = []
+var cardsWon = []
 
 // gameboard
 
@@ -52,7 +56,7 @@ function createBoard(){
         var card = document.createElement('img')
         card.setAttribute('src', 'images/blank.png')
         card.setAttribute('data-id', i)
-        // card.addEventListener('click', flipCard)
+        card.addEventListener('click', flipCard)
         grid.appendChild(card)
     }
 }
@@ -62,27 +66,37 @@ function checkForMatch(){
     var cards = document.querySelectorAll('img')
     const optionOneId = cardsChosenId[0]
     const optionTwoId = cardsChosenId[1]
-    if( cardsChosen[0] === cardsChosen[1])
+
+    if( cardsChosen[0] === cardsChosen[1]){
     alert('Tou have found a match')
     cards[optionOneId].setAttribute('src', 'images/white.png')
     cards[optionTwoId].setAttribute('src', 'images/white.png')
+    cardsWon.push(cardsChosen)
+    }else{
+        cards[optionOneId].setAttribute('src', 'images/blank.png')
+        cards[optionTwoId].setAttribute('src', 'images/blank.png')
+        alert("Sorry, try again")
+    }
+    cardsChosen = []
+    cardsChosenId = []
+    resultDisplay.textContent = cardsWon.length
+    if(cardsWon.length === cardArray.length/2){
+        resultDisplay.textContent = 'Congratulations you have found them all!'
+    }
 }
+
 
 // flip the card
 function flipCard(){
-    var cardId = this.getAttribute(data-id)
+    var cardId = this.getAttribute('data-id')
     cardsChosen.push(cardArray[cardId].name)
     cardsChosenId.push(cardId)
     this.setAttribute('src', cardArray[cardId].img)
-    if(cardsChosen.length = 2){
+    if(cardsChosen.length === 2){
         setTimeout(checkForMatch, 500)
     }
 }
 
 createBoard()
-
-
-
-
 
 })
